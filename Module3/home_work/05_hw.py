ad0fe5a03ee2641f8105cf3268a17002c8bb143b
ShlyapaPython <88583725+ShlyapaPython@users.noreply.github.com>
2021-08-19 22:50:22 +0300

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -4,3 +4,10 @@
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 # TODO: your code here
+names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
+max_l = 1
+for el in names:
+    if len(el) > max_l:
+        max_l = len(el)
+        max_name = el
+print (max_name)
